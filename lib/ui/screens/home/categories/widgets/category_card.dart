e419b96dc5fcff920cd06a97aa794fa3201cb7e0
Silas Ribeiro <santorsilas@gmail.com>
2024-09-15 22:45:46 -0300

    ui: update category card style

--- lib/ui/screens/home/categories/widgets/category_card.dart
+++ lib/ui/screens/home/categories/widgets/category_card.dart
@@ -10,25 +10,40 @@ class CategoryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Image.network(
-          category.imageUrl,
-          height: 253,
-        ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Text(
-            category.name,
-            textAlign: TextAlign.center,
-            style: context.appTextStyles.headlineSmall.copyWith(
-              color: context.appColors.darkBlue,
-              fontWeight: FontWeight.w700,
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Color(0x267B7B7B),
+            offset: Offset(0, 4),
+            blurRadius: 24,
+            spreadRadius: 0,
+          ),
+        ],
+      ),
+      margin: const EdgeInsets.only(bottom: 24.0),
+      child: Column(
+        children: [
+          Image.network(
+            category.imageUrl,
+            height: 253,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              category.name,
+              textAlign: TextAlign.center,
+              style: context.appTextStyles.headlineSmall.copyWith(
+                color: context.appColors.darkBlue,
+                fontWeight: FontWeight.w700,
+              ),
             ),
           ),
-        ),
-        const SizedBox(height: 24.0),
-      ],
+        ],
+      ),
     );
   }
 }
