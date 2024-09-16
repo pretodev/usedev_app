3de38a00fea5f0e2c205349ade1e1aed3b05b578
Silas Ribeiro <santorsilas@gmail.com>
2024-09-15 22:23:49 -0300

    ui: update PromotionalProductItem layout and styles

--- lib/ui/screens/home/promotion_products/widgets/promotional_product_item.dart
+++ lib/ui/screens/home/promotion_products/widgets/promotional_product_item.dart
@@ -10,38 +10,55 @@ class PromotionalProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Image.network(
-          product.imageUrl,
-          height: 253,
-        ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                product.name,
-                textAlign: TextAlign.left,
-                style: context.appTextStyles.titleLarge.copyWith(
-                  color: context.appColors.darkBlue,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              Text(
-                product.price.toString(),
-                textAlign: TextAlign.left,
-                style: context.appTextStyles.titleSmall.copyWith(
-                  color: context.appColors.darkBlue,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-            ],
+    return Container(
+      margin: const EdgeInsets.only(bottom: 24.0),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Color(0x1F780BF7),
+            offset: Offset(0, 4),
+            blurRadius: 24,
+            spreadRadius: 0,
           ),
-        ),
-        const SizedBox(height: 24.0),
-      ],
+        ],
+      ),
+      child: Column(
+        children: [
+          Image.network(
+            product.imageUrl,
+            height: 253,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            color: context.appColors.mediumGray2,
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  product.name,
+                  textAlign: TextAlign.left,
+                  style: context.appTextStyles.headlineSmall.copyWith(
+                    color: context.appColors.darkBlue,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                Text(
+                  product.price.toString(),
+                  textAlign: TextAlign.left,
+                  style: context.appTextStyles.titleSmall.copyWith(
+                    color: context.appColors.darkBlue,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
